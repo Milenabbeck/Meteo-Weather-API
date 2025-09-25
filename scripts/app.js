5b3ef2baa4fa7b9bced530bd67b950ee1012f19d
Milena Beeck <milenasperia@gmail.com>
2025-09-25 12:06:37 -0300

    Reestruturando meu código e acrescentando campo de input no HTML

--- scripts/app.js
+++ scripts/app.js
@@ -1,55 +1,57 @@
-let latitude = "52.52";
-let longitude = "13.41";
 let current = "temperature_2m";
-let timezone = "America/Sao_Paulo";
+let timezone = "Europe/Sao_Paulo";
+
+let latitude = document.getElementById('latitude');
+let longitude = document.getElementById('longitude');
 
 let button = document.getElementById('btnWeather');
 let statusInfo = document.getElementById('status');
 let div = document.getElementById('out');
 
 button.addEventListener('click', function () {
+    updateStatus("Carregando...");
+
+    fetchWeather()
+        .then(function (json) {
+            let current = json.current;
+            let weather = Weather.fromRaw(current);
+            div = weather.renderFrom(div);
+        })
+        .catch(function (error) {
+            renderError(error);
+        })
+        .finally(() => updateStatus(""));
+});
+
+function fetchWeather() {
     let params = new URLSearchParams({
-        latitude: latitude,
-        longitude: longitude,
+        latitude: latitude.value,
+        longitude: longitude.value,
         current: current,
         timezone: timezone
-    })
+    });
 
     let url = `https://api.open-meteo.com/v1/forecast?${params.toString()}`;
 
-    uptadeStatus("Carregando...");
+    return fetch(url)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                let errorMessage = "Erro: Recurso não encontrado ou servidor não disponível. Tente novamente.";
+                throw new Error(errorMessage);
+            }
+        });
+}
 
-    fetch(url)
-    .then(function (response) {
-        if (response.ok) {
-            return response.json();
-        } else {
-            let errorMessage = "Erro: Recurso não encontrado."
-            throw new Error(errorMessage);
-        }
-    })
-    .then(function (json) {
-        let current = json.current;
-        let weather = Weather.fromRaw(current);
-        div = weather.renderFrom(div);
-    })
-    .catch(function (error) {
-        renderError(error);
-    })
-    // .finally(function () {
-    //     uptadeStatus("");
-    // }) // Versão com função anônima
-    .finally(() => uptadeStatus(""))
-});
-
-function uptadeStatus(msg) {
+function updateStatus(msg) {
     statusInfo.textContent = msg || "";
 }
 
 function renderError(error) {
     div.innerHTML = "";
 
-    let h2 = document.createElement(`h2`);
+    let h2 = document.createElement('h2');
     h2.textContent = error;
 
     div.appendChild(h2);
